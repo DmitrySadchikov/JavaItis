124575fcf8183b6d6e3fa44665ff46d9dfdcdeb9
SadchikovDmitry <dmi.sadchikov@gmail.com>
2016-10-13 20:52:50 +0300

    home work

--- JdbsDemo/src/main/java/ru/itis/dao/CarsDaoJdbcImpl.java
+++ JdbsDemo/src/main/java/ru/itis/dao/CarsDaoJdbcImpl.java
@@ -2,16 +2,26 @@ package ru.itis.dao;
 
 import ru.itis.models.Car;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CarsDaoJdbcImpl implements CarsDao {
 
     private Connection connection;
+
+    //language=SQL
+    private static final String SQL_ALL_CARS = "SELECT * FROM cars";
+    //language=SQL
+    private static final String SQL_FIND_CARS = "SELECT * FROM cars WHERE id = ?";
+    //language=SQL
+    private static final String SQL_DELETE_CARS = "DELETE FROM cars WHERE id = ?";
+    //language=SQL
+    private static final String SQL_UPDATE_CARS = "UPDATE cars SET car_make = ?, " +
+            "mileage = ?, power = ?, WHERE id = ?;";
+    //language=SQL
+    private static final String SQL_ADD_CARS = "INSERT INTO cars (car_make, mileage, power)"
+            + " VALUES (?, ?, ?);";
 
     public CarsDaoJdbcImpl(Connection connection) {
         this.connection = connection;
@@ -23,7 +33,7 @@ public class CarsDaoJdbcImpl implements CarsDao {
             List<Car> cars = new ArrayList<Car>();
             Statement statement = connection.createStatement();
 
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM car_users");
+            ResultSet resultSet = statement.executeQuery(SQL_ALL_CARS);
             while (resultSet.next()) {
                 Car c = new Car(resultSet.getInt("id"), resultSet.getString("car_make"),
                         resultSet.getInt("mileage"), resultSet.getInt("power"));
@@ -37,8 +47,9 @@ public class CarsDaoJdbcImpl implements CarsDao {
 
     public Car find(int id) {
         try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM cars WHERE id=" + id);
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_FIND_CARS);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
             resultSet.next();
             Car c = new Car(resultSet.getInt("id"), resultSet.getString("car_make"),
                     resultSet.getInt("mileage"), resultSet.getInt("power"));
@@ -50,8 +61,9 @@ public class CarsDaoJdbcImpl implements CarsDao {
 
     public void delete(int id) {
         try {
-            Statement statement = connection.createStatement();
-            statement.executeQuery("DELETE * FROM cars WHERE id=" + id);
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_DELETE_CARS);
+            preparedStatement.setInt(1, id);
+            preparedStatement.execute();
         } catch (SQLException e) {
             throw new IllegalArgumentException(e);
         }
@@ -60,10 +72,12 @@ public class CarsDaoJdbcImpl implements CarsDao {
     public void update(Car car) {
 
         try {
-            Statement statement = connection.createStatement();
-            statement.executeQuery("UPDATE cars SET car_make=" + car.getMake()
-                    + ", mileage=" + car.getMileage() + ", power=" + car.getPower()
-                    + " WHERE id=" + car.getId());
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_UPDATE_CARS);
+            preparedStatement.setString(1, car.getMake());
+            preparedStatement.setInt(2, car.getMileage());
+            preparedStatement.setInt(3, car.getPower());
+            preparedStatement.setInt(4, car.getId());
+            preparedStatement.execute();
         } catch (SQLException e) {
             throw new IllegalArgumentException(e);
         }
@@ -71,10 +85,11 @@ public class CarsDaoJdbcImpl implements CarsDao {
 
     public void add(Car car) {
         try {
-            Statement statement = connection.createStatement();
-            statement.executeQuery("INSERT INTO cars (car_make, mileage, power)" +
-                    " VALUES (" + car.getMake() + ", " + car.getMileage() + ", "
-                    + car.getPower() + ");");
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_ADD_CARS);
+            preparedStatement.setString(1, car.getMake());
+            preparedStatement.setInt(2, car.getMileage());
+            preparedStatement.setInt(3, car.getPower());
+            preparedStatement.execute();
         } catch (SQLException e) {
             throw new IllegalArgumentException(e);
         }
